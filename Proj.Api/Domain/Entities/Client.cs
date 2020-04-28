db875ff61c2cf951fc0ad5321b5f30860830b01b
aldrinjunior <aldrinjunior@gmail.com>
2020-04-28 14:25:28 -0300

    API Pronta -correção dateTime

--- Proj.Api/Domain/Entities/Client.cs
+++ Proj.Api/Domain/Entities/Client.cs
@@ -2,15 +2,12 @@ using System;
 using System.Collections.Generic;
 using Proj.Api.Domain.Entities;
 
-namespace Proj.Api.Domain
-{
-  public class Client
-  {
+namespace Proj.Api.Domain {
+  public class Client {
 
-    public Client() { }
+    public Client () { }
 
-    public Client(int id, string name, string cpfCnpj, DateTime nascimento, string sexo, string inscEstadual, string profissao, ICollection<Address> address)
-    {
+    public Client (int id, string name, string cpfCnpj, string nascimento, string sexo, string inscEstadual, string profissao, ICollection<Address> address) {
       this.id = id;
       this.name = name;
       this.cpfCnpj = cpfCnpj;
@@ -24,7 +21,7 @@ namespace Proj.Api.Domain
     public int id { get; set; }
     public string name { get; set; }
     public string cpfCnpj { get; set; }
-    public DateTime nascimento { get; set; }
+    public string nascimento { get; set; }
     public string sexo { get; set; }
     public string inscEstadual { get; set; }
     public string profissao { get; set; }
