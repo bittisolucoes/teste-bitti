729b5734da7e9c6dac36a3a2f42d89cfcb790f87
Lucas da Silva Silveira <lucas.silva.silveira@rede.ulbra.br>
2020-05-01 15:23:56 -0300

    teste bitti

---
+++ Api/TesteBitti.Repositories/Interfaces/IClienteRepository.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using TesteBitti.Domain;
+namespace TesteBitti.Repositories.Interfaces
+{
+    public interface IClienteRepository : IRepositoryBase<Cliente>
+    {
+      
+    }
+}
---
+++ Api/TesteBitti.Repositories/Migrations/20200424041756_initial migration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TesteBitti.Repositories.Migrations
+{
+    public partial class initialmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    CpfCnpj = table.Column<string>(nullable: true),
+                    DataNascimento = table.Column<DateTime>(nullable: false),
+                    Sexo = table.Column<string>(nullable: true),
+                    InscricaoEstadual = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cliente");
+        }
+    }
+}
