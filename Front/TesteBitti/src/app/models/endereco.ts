c2804e8a313e54cdf9180a0cc53711d9469b40a5
Lucas da Silva Silveira <lucas.silva.silveira@rede.ulbra.br>
2020-05-04 00:28:04 -0300

    final-front commit

---
+++ Front/TesteBitti/src/app/models/endereco.ts
@@ -0,0 +1,12 @@
+import { Cliente } from './cliente';
+
+export class Endereco{
+    id: number;
+    cep: string;
+    logradouro:string;
+    numero:number;
+    bairro:string;
+    cidade:string;
+    estado:string;
+    complemento:string;
+}
